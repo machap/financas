e88e7f8f15b8fa704f4823e5a80266f0bd6f6a96
machap <paulinhodj10@gmail.com>
2018-10-20 00:23:00 -0300

    Consultando tabela ManyToMany

--- src/br/com/caelum/financas/teste/TesteMovimentacoesPorCategoria.java
+++ src/br/com/caelum/financas/teste/TesteMovimentacoesPorCategoria.java
@@ -5,32 +5,25 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
-import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Categoria;
 import br.com.caelum.financas.modelo.Movimentacao;
-import br.com.caelum.financas.modelo.TipoMovimentacao;
 import br.com.caelum.financas.util.JPAUtil;
 
-public class TesteJPQL {
+public class TesteMovimentacoesPorCategoria {
 
 	public static void main(String[] args) {
-
+		
 		EntityManager em = new JPAUtil().getEntityManager();
 		em.getTransaction().begin();
 
-		Conta conta = new Conta();
-		conta.setId(2);
-
-		// String query = "select m from Movimentacao m where conta_id = 2";
-		// String jpql = "select m from Movimentacao m where m.conta.id = 2";
-
-		String jpql = "select m from Movimentacao m where m.conta = :pConta" + " and m.tipo = :pTipo"
-				+ " order by m.valor desc";
-
+		Categoria categoria = new Categoria();
+		categoria.setId(1);
+		
+		String jpql = "select m from Movimentacao m join m.categorias c where c = :pCategoria";
+		
 		Query query = em.createQuery(jpql);
-		// São os paremetros passado na query, comparando as entidades.
-		query.setParameter("pConta", conta);
-		query.setParameter("pTipo", TipoMovimentacao.SAIDA);
-
+		query.setParameter("pCategoria", categoria);
+		
 		List<Movimentacao> resultados = query.getResultList();
 
 		for (Movimentacao movimentacao : resultados) {
